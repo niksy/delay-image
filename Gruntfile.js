45166cfadb6473484d3e1f76c7ddda9b36589971
Ivan Nikolić <niksy5@gmail.com>
2014-03-31 23:09:42 +0200

    Update Gruntfile
    Add "stylecheck" to default process.

--- Gruntfile.js
+++ Gruntfile.js
@@ -98,7 +98,7 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks( 'grunt-bump' );
 
 	grunt.registerTask( 'stylecheck', ['jshint:main', 'jscs:main'] );
-	grunt.registerTask( 'default', ['concat:main', 'uglify:main', 'concat:withDeps'] );
+	grunt.registerTask( 'default', ['stylecheck', 'concat:main', 'uglify:main', 'concat:withDeps'] );
 	grunt.registerTask( 'releasePatch', ['bump-only:patch', 'default', 'bump-commit'] );
 	grunt.registerTask( 'releaseMinor', ['bump-only:minor', 'default', 'bump-commit'] );
 	grunt.registerTask( 'releaseMajor', ['bump-only:major', 'default', 'bump-commit'] );
